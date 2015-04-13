05ebab9d15d55523ed27fb3228fc795731aab76f
Simon <simwmobile@gmail.com>
2015-04-13 14:21:42 +0200

    Nettoyage des nouvelles fonctions et commentaires

--- application/controllers/Messages.php
+++ application/controllers/Messages.php
@@ -33,38 +33,24 @@ class Messages extends CI_Controller
         $data['nom'] = "";
         $data['urlevenement'] = $urlevenement;
 
-        $this->load->view('templates/header', $data);
+        //Charge les modules pour les formulaires et leur validation
         $this->load->helper(array('form', 'url'));
 
         $this->load->library('form_validation');
 
+        //Si on a soumis le formulaire
         if($this->input->post('submit')){
-            //$this->books_model->entry_insert();
-            echo $this->input->post('nom');
-            echo $this->input->post('message');
 
             //On récupère l'ID de l'évenement grace a son URL
             $IDEvenement = $this->messages_model->getIDFromURL($urlevenement);
-            echo $IDEvenement['IDEvenement'];
-
-            //$this->load->view('messages/post'.$urlevenement, $data);
 
             //On insère le message
             $this->messages_model->postMessage($IDEvenement['IDEvenement'], $this->input->post('nom'), $this->input->post('message'));
 
 
         }
-/*
-        if ($this->form_validation->run() == FALSE)
-        {
-            $this->load->view('messages/post',$data);
-        }
-        else
-        {
-            $this->load->view('messages/post/'.$urlevenement,$data);
-        }
-*/
 
+        $this->load->view('templates/header', $data);
         $this->load->view('messages/post',$data);
         $this->load->view('templates/footer');
 
--- application/views/messages/post.php
+++ application/views/messages/post.php
@@ -11,26 +11,21 @@ echo $urlevenement;
 echo form_open('messages/post/'.$urlevenement);
 
 echo form_label("Votre nom", "nom");
-echo form_input('nom', ''); ?>
+echo form_input('nom', '');
+?>
 
 </br>
 
 <?php
 echo form_label("Votre message", "message");
 echo form_textarea('message','');
+?>
 
-//echo form_label($contact_form["email"]["label"], $contact_form["email"]["field"]);
-//echo form_input($contact_form["email"]["field"], '');
+</br>
 
-//echo form_label($contact_form["subject"]["label"], $contact_form["subject"]["field"]);
-//echo form_input($contact_form["subject"]["field"], '');
-
-//echo form_label($contact_form["message"]["label"], $contact_form["message"]["field"]);
-//echo form_input($contact_form["message"]["field"], '');
-
+<?php
 echo form_submit('submit', 'Submit');
 echo form_close();
-
 ?>
 
 </body>
