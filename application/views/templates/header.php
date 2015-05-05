bf166b206b9a764b039c14994dff0b0e49a09953
p0linee <pauline.mendez@etu.u-bordeaux.fr>
2015-05-05 16:06:16 +0200

    carousel

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -4,6 +4,16 @@
                 <link rel="stylesheet" href="/assets/css/bootstrap.css" >
                 <link rel="stylesheet" href="/assets/css/bootstrap-theme.css">
                 <link rel="stylesheet" media="screen" type="text/css" href="/assets/css/colorpicker.css" >
+                <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+                <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+                <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+                <style>
+                    .carousel-inner > .item > img,
+                    .carousel-inner > .item > a > img {
+                        width: 70%;
+                        margin: auto;
+                    }
+                </style>
         </head>
 
         <body>
