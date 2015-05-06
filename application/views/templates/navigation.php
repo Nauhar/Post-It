2ddb5cea206d7af5c4374375c343866d751b5d91
Simon <simwmobile@gmail.com>
2015-05-06 02:35:58 +0200

    Ajout non de la personne connecté dans la navbar

--- application/views/templates/navigation.php
+++ application/views/templates/navigation.php
@@ -12,7 +12,7 @@
 
             <ul class="nav navbar-nav navbar-right">
                 <?php if (isset($this->session->userdata['logged_in'])) { ?>
-                <li><a href ="/index.php/users/logout"><span class ="glyphicon glyphicon-log-out"></span> Déconnexion</a></li>
+                <li><p class="navbar-text" style="margin: 0px"><?php echo $this->session->userdata['NomUtilisateur']." ".$this->session->userdata['PrenomUtilisateur'];?></p><a href ="/index.php/users/logout"><span class ="glyphicon glyphicon-log-out"></span> Déconnexion</a></li>
                 <?php }else{?>
                 <li><a href="/index.php/users/login"><span class="glyphicon glyphicon-log-in"></span> S'identifier</a></li>
                 <li><a href="/index.php/users/inscription"><span class="glyphicon glyphicon-leaf"></span> Créer un compte</a></li>
